1bb1b3d6c233634166da58d4d5f9893788d54116
sumedha <sumedhanarayanan@gmail.com>
2019-01-16 01:48:57 +0000

    Accepts form input and creates an entry in the URL database

--- express_server.js
+++ express_server.js
@@ -24,35 +24,43 @@ function checkUniqueShortUrl(randomString) {
   return true;
 }
 
+// Database that stores short and long url pairs
 var urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+// Root path 
 app.get('/', (request, response) => {
   response.send('Hello!');
 });
+//Path that returns current JSON of URLS
 app.get('/urls.json', (request, response) => {
   response.json(urlDatabase);
 });
+// Path that lists the url index
 app.get('/urls', (request, response) => {
   response.render('urls_index', {urls: urlDatabase});
 });
+//Path to generate a new tiny URL
 app.get('/urls/new', (request, response) => {
   response.render('urls_new');
 });
+//shows the short and long URL for a specific short URL
 app.get('/urls/:id', (request, response) => {
   response.render('urls_show', {shortURL: request.params.id,
                                 longURL: urlDatabase[request.params.id] });
 });
 
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+//Post function to accept a new long URL , gneerate a random shortURL and then add to database
+app.post("/urls", (request, response) => {
+  //request.body uses the package body parser to encode it in the key value pairs defined in the form tag
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = request.body.longURL;
+  response.send("Ok");         
 });
 
 
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
