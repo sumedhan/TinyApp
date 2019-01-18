b5b04030458b876064bf1bde91a316fe250d18d9
sumedha <sumedhanarayanan@gmail.com>
2019-01-18 03:34:56 +0000

    Added cookie session and tidied up formatting for url index

--- express_server.js
+++ express_server.js
@@ -3,16 +3,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
 const moment = require('moment');
 const bcrypt = require('bcrypt');
-
+const cookieSession = require('cookie-session')
 
 //Middleware
 app.set('view engine', 'ejs') 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
 app.use(express.static('public'));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['dobo986', 'jene787'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 
 //Function that generates a random string for the shortened URL. The url is 6 characters long.
@@ -43,11 +48,12 @@ function canUserBeRegistered(email) {
 
 // function to register a new user in the user database
 function registeruserId(email, password) {
-  const uniqueUserId = generateRandomString();
+  let uniqueUserId = generateRandomString();
+  let hashedPassword = hashPassword(password);
   users[uniqueUserId] = {
     id: uniqueUserId,
     email,
-    password
+    hashedPassword
   };
   return uniqueUserId;
 }
@@ -71,8 +77,8 @@ function findUserId(email) {
 
 // function to match the entered email and password to the database entries
 function passwordMatch(email, password) {
-  const userId = findUserId(email);
-  if(password === users[userId].password)
+  let userID = findUserId(email);
+  if(checkPassword(password, userID))
     return true;
   else 
     return false;
@@ -107,19 +113,20 @@ function hashPassword(password) {
 function checkPassword(password, userID) {
   return bcrypt.compareSync(password, users[userID].password);
 }
+
 // Database that stores short and long url pairs
 var urlDatabase = {
   'b2xVn2': 
   {
     longURL: 'http://www.lighthouselabs.ca',
-    userID: "userRandomID",
+    userID: "abcde",
     dateCreated: "Jan 17th 2019",
     numberOfVisits: 0
   },
   '9sm5xK':
   {
     longURL: 'http://www.google.com',
-    userID: "user2RandomID",
+    userID: "dobo",
     dateCreated: "Jan 17th 2018",
     numberOfVisits: 0
   }
@@ -127,16 +134,16 @@ var urlDatabase = {
 
 // User database
 const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+  "abcde" : {
+    id: "abcde",
+    email: "a@b.com",
+    password: hashPassword("123")
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
+  "dobo" : {
+    id: "dobo",
+    email: "d@dobo.com",
+    password: hashPassword("sume")
+  },
 }
 
 
@@ -144,37 +151,38 @@ const users = {
 
 // Root path 
 app.get('/', (request, response) => {
-  if(isUserLoggedIn(request.cookies.user_id))
+  if(isUserLoggedIn(request.session.user_id))
     response.redirect('/urls');
   else 
     response.redirect('/login');
 });
 
-//Login page
-app.get('/login', (request, response) => {
-  if(isUserLoggedIn(request.cookies.user_id))
-    response.redirect('/urls');
-  else
-    response.render('user_login');
-});
-
 // Path that lists the url index. If logged out, redirects to login page
 app.get('/urls', (request, response) => {
-  if(isUserLoggedIn(request.cookies.user_id)) {
-    console.log(users[request.cookies.user_id]);
+  if(isUserLoggedIn(request.session.user_id)) {
     response.render('urls_index', {
       urls: urlDatabase,
-      user: users[request.cookies.user_id]
+      user: users[request.session.user_id]
     });
   } else {
     response.redirect('/login');
   }
 });
 
+//Login page
+app.get('/login', (request, response) => {
+  if(isUserLoggedIn(request.session.user_id))
+    response.redirect('/urls');
+  else
+    response.render('user_login');
+});
+
+
+
 //Path to generate a new tiny URL
 app.get('/urls/new', (request, response) => {
-  if(isUserLoggedIn(request.cookies.user_id)) {
-    response.render('urls_new', {user: users[request.cookies.user_id]});
+  if(isUserLoggedIn(request.session.user_id)) {
+    response.render('urls_new', {user: users[request.session.user_id]});
   } else {
     response.redirect('/login');
   }
@@ -186,7 +194,7 @@ app.get('/urls/:id', (request, response) => {
   response.render('urls_show', {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL].longURL,
-    user: users[request.cookies.user_id]
+    user: users[request.session.user_id]
   });
 });
 
@@ -217,9 +225,9 @@ app.get("/u/:shortURL", (request, response) => {
 
 //Post function to accept a new long URL , gneerate a random shortURL and then add to database
 app.post("/urls", (request, response) => {
-  if(isUserLoggedIn(request.cookie.user_id)) {
+  if(isUserLoggedIn(request.session.user_id)) {
     let longURL = request.body.longURL;
-    let userID = request.cookie.user_id;
+    let userID = request.session.user_id;
     let shortURL = createURL(longURL, userID);
     response.redirect(`/urls/${shortURL}`);  
   } else {
@@ -231,7 +239,7 @@ app.post("/urls", (request, response) => {
 //Deletes URLS from database
 app.post("/urls/:id/delete", (request, response) => {
   let shortURL = request.params.id;
-  let userID = request.cookies.user_id;
+  let userID = request.session.user_id;
   if(urlBelongsToUser(shortURL, userID)) {
     delete urlDatabase[shortURL];
     response.redirect("/urls");
@@ -244,7 +252,7 @@ app.post("/urls/:id/delete", (request, response) => {
 //Updates URLs in database
 app.post("/urls/:id", (request, response) => {
   let shortURL = request.params.id;
-  let userID = request.cookies.user_id;
+  let userID = request.session.user_id;
   if(urlBelongsToUser(shortURL, userID)) {
     urlDatabase[shortURL].longURL = request.body.updatedLongURL;
     response.redirect("/urls");
@@ -269,14 +277,14 @@ app.post("/login", (request, response) => {
     response.statusCode = 403;
     response.send("Incorrect username/password");
   } else {
-    response.cookie("user_id", findUserId(email));
+    request.session.user_id = findUserId(email);
     response.redirect("/urls");
   }
 });
 
 //log out the user
 app.post("/logout", (request, response) => {
-  response.clearCookie("user_id");
+  request.session = null;
   response.redirect("/urls");
 });
 
@@ -292,7 +300,7 @@ app.post("/register", (request, response) => {
     response.send('User already exists');
   } else {
     const userId = registeruserId(email, password);
-    response.cookie("user_id", userId);
+    request.session.user_id = userId;
     response.redirect('/urls');
   }
 })
